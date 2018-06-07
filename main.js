ce5decb90e61eb54229f0575b4028ef217e03a15
wuwenxing <1428560819@qq.com>
2018-06-07 15:44:12 +0800

    无缝轮播

---
+++ main.js
@@ -0,0 +1,43 @@
+var n = 1
+init()
+
+setInterval(() => {
+
+    leave(getImg(n)).one('transitionend', (e) => {
+        enter($(e.currentTarget))
+    });
+    current(getImg(n + 1))
+    n++
+}, 3000)
+
+function getImg(n) {
+    return $(`.window>.images>img:nth-child(${x(n)})`)
+}
+
+function init() {
+    getImg(n).addClass('current').siblings().addClass('enter')
+}
+
+function current(node) {
+    return node.removeClass('enter leave').addClass('current')
+}
+
+function leave(node) {
+    return node.removeClass('enter current').addClass('leave')
+}
+
+function enter(node) {
+    return node.removeClass('currnet leave').addClass('enter')
+}
+
+
+
+function x(n) {
+    if (n > 5) {
+        n = n % 5
+        if (n === 0) {
+            n = 5
+        }
+    }
+    return n
+}
