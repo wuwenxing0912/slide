30f442f7d915df73d0aed719d09d89ececddf7ee
wuwenxing <1428560819@qq.com>
2018-06-14 01:06:44 +0800

    第三次修改

--- main.js
+++ main.js
@@ -1,57 +1,90 @@
-var n = 1
-init()
+var slides = $('#slides')
+var buttons = $('#buttonWrapper>button')
+var images = slides.children('img')
+var current = 0
 
-var timer = setTimer()
+makeFakeSlides()
+slides.css({ transform: 'translateX(-400px)' })
+bindEvents()
 
-function setTimer() {
-    return setInterval(() => {
+$('#previous').on('click', function() {
+    goToSlide(current - 1)
+})
+$('#next').on('click', function() {
+    goToSlide(current + 1)
+})
 
-        leave(getImg(n)).one('transitionend', (e) => {
-            enter($(e.currentTarget))
-        });
-        current(getImg(n + 1))
-        n++
-    }, 3000)
-}
+var timer = setInterval(function() {
+    goToSlide(current + 1)
+    smallImg(current)
+}, 2000)
 
-
-document.addEventListener('visibilitychange', function(e) {
+document.addEventListener('visibilitychange', function() {
     if (document.hidden) {
         window.clearInterval(timer)
     } else {
-        timer = setTimer()
+        timer = setInterval(function() {
+            goToSlide(current + 1)
+            smallImg(current)
+        }, 2000)
     }
 })
 
+$('.container').on('mouseenter', function() {
+    window.clearInterval(timer)
+}).on('mouseleave', function() {
+    timer = setInterval(function() {
+        goToSlide(current + 1)
+        smallImg(current)
+    }, 2000)
+})
 
-function getImg(n) {
-    return $(`.window>.images>img:nth-child(${x(n)})`)
+function makeFakeSlides() {
+    var copyFirst = images.eq(0).clone(true)
+    var copyLast = images.eq(images.length - 1).clone(true)
+    slides.append(copyFirst);
+    slides.prepend(copyLast)
 }
 
-function init() {
-    getImg(n).addClass('current').siblings().addClass('enter')
+function bindEvents() {
+    $('#buttonWrapper').on('click', 'button', function(e) {
+        var button = $(e.currentTarget)
+        var index = button.index()
+        goToSlide(index)
+        smallImg(index)
+    })
 }
 
-function current(node) {
-    return node.removeClass('enter leave').addClass('current')
+function smallImg(index) {
+    buttons.eq(index).addClass('active').siblings('button').removeClass('active')
 }
 
-function leave(node) {
-    return node.removeClass('enter current').addClass('leave')
-}
-
-function enter(node) {
-    return node.removeClass('currnet leave').addClass('enter')
-}
-
-
-
-function x(n) {
-    if (n > 5) {
-        n = n % 5
-        if (n === 0) {
-            n = 5
-        }
+function goToSlide(index) {
+    if (index > buttons.length - 1) {
+        index = 0
+    } else if (index < 0) {
+        index = buttons.length - 1
     }
-    return n
+
+    console.log('current', 'index')
+    console.log(current, index)
+
+    if (current === buttons.length - 1 && index === 0) {
+        //最后一张到第一张
+        slides.css({ transform: `translateX(${-(buttons.length + 1 )*400}px)` }).
+        one('transitionend', function() {
+            slides.hide().offset()
+            slides.css({ transform: `translateX(${-(index + 1 )*400}px)` }).show()
+        })
+    } else if (current === 0 && index === buttons.length - 1) {
+        //第一张到最后一张
+        slides.css({ transform: 'translateX(0px)' }).
+        one('transitionend', function() {
+            slides.hide().offset()
+            slides.css({ transform: `translateX(${-(index + 1 )*400}px)` }).show()
+        })
+    } else {
+        slides.css({ transform: `translateX(${-(index + 1 )*400}px)` })
+    }
+    current = index
 }
